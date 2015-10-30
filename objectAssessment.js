691bb3a01a2b55946abebda80771500d8c879b1c
cwkwebdesign <cwk.webdesign@gmail.com>
2015-10-30 00:24:13 -0500

    Initial Commit

---
+++ objectAssessment.js
@@ -0,0 +1,16 @@
+/* Original file
+avengersMovie = {
+	name : Avengers;
+	runtime : 143 mins;
+	releaseYear = 2012;
+	"Director" : "Joss Whedon".
+}
+*/
+
+//Updated file
+var avengersMovie = {
+	name: "Avengers",
+	minutesLong: 143,
+	releaseYear: 2012,
+	director: "Joss Whedon"
+};
